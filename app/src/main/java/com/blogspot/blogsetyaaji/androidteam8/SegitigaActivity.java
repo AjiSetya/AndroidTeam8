9ad11aeb0a122afaa027db9daf5d5fd6fee7a03f
afiq <afiq3821@gmail.com>
2018-10-08 11:31:43 +0700

    update segitiga

--- app/src/main/java/com/blogspot/blogsetyaaji/androidteam8/SegitigaActivity.java
+++ app/src/main/java/com/blogspot/blogsetyaaji/androidteam8/SegitigaActivity.java
@@ -8,6 +8,6 @@ public class SegitigaActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.Segitiga);
     }
 }
