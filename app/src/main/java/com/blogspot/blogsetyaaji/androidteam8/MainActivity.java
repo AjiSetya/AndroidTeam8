b50de313f5e269b9f209438387f7ac653e930243
Farid Savarudin <farid@dycode.com>
2018-10-27 22:19:24 +0700

    add button

--- app/src/main/java/com/blogspot/blogsetyaaji/androidteam8/MainActivity.java
+++ app/src/main/java/com/blogspot/blogsetyaaji/androidteam8/MainActivity.java
@@ -3,8 +3,11 @@ package com.blogspot.blogsetyaaji.androidteam8;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.awt.Button;
+
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
