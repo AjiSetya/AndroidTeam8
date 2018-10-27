840bff6a622efa3f37c8dc10b4f42514a2946041
Farid Savarudin <farid@dycode.com>
2018-10-27 22:20:21 +0700

    imageview

--- app/src/main/java/com/blogspot/blogsetyaaji/androidteam8/MainActivity.java
+++ app/src/main/java/com/blogspot/blogsetyaaji/androidteam8/MainActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 
 import java.awt.Button;
 
+import javax.swing.text.html.ImageView;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
