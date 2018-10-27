13fc2e5d10dafcff9b76899bb19dad36573ddfb6
Farid Savarudin <farid@dycode.com>
2018-10-27 22:11:07 +0700

    test

--- app/src/main/java/com/blogspot/blogsetyaaji/androidteam8/HitungPersegi.java
+++ app/src/main/java/com/blogspot/blogsetyaaji/androidteam8/HitungPersegi.java
@@ -3,7 +3,10 @@ package com.blogspot.blogsetyaaji.androidteam8;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.awt.Button;
+
 public class HitungPersegi extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
